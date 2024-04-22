874068da7d5856e4e0cc305e2b56b2e136921ac9
Kimdekker <kim.dekker@q42.nl>
2024-04-22 15:46:03 +0200

    buggs

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,47 +1,48 @@
 import { Canvas, useFrame } from '@react-three/fiber';
-import { useEffect, useLayoutEffect, useRef } from 'react';
-import { OrbitControls, PerspectiveCamera, useScroll } from '@react-three/drei';
+import { useLayoutEffect, useRef } from 'react';
+import { OrbitControls, PerspectiveCamera, ScrollControls, useScroll } from '@react-three/drei';
 import Office from '../components/Office';
-import gsap from 'gsap';
+import { gsap } from 'gsap';
+
+
+export const FLOOR_HEIGHT = 2.3;
+export const NB_FLOORS = 3;
 
 const HomePage = () => {
 
   const cameraRef = useRef();
   const trailRef = useRef();
   const tl = useRef();
+  console.log('timeline: ' + tl); 
   const scroll = useScroll();
 
 
   const FrameUpdate = () => {
     useFrame(() => {
+      console.log('useFrame is running');
       if (tl.current) {
+        console.log('tl.current exists'); 
         tl.current.seek(scroll.offset * tl.current.duration());
+        console.log(scroll.offset); 
       }
     });
   
     useLayoutEffect(() => {
+      console.log('useLayoutEffect is running');
       if (trailRef.current) {
+        console.log('trailRef.current exists');
         tl.current = gsap.timeline();
-    
-      // Camera movement
-      tl.current.from(
-        trailRef.current.position,
-        {
-          duration: 0.5,
-          x: -2,
-        },
-        0.5
-      );
-      tl.current.from(
-        trailRef.current.rotation,
-        {
-          duration: 0.5,
-          y: Math.PI / 2,
-        },
-        0
-      );
 
-
+        console.log(gsap.timeline());
+        // VERTICAL ANIMATION
+        tl.current.to(
+          trailRef.current.position,
+          {
+            duration: 2,
+            y: -FLOOR_HEIGHT * (NB_FLOORS - 1),
+          },
+          0
+        );
       }
     }, []);
   
@@ -49,29 +50,29 @@ const HomePage = () => {
   };
 
 
-
   return (
-      <Canvas camera={{fov: 64, position: [2.3, 1.5, 2.3], }}>
+      <Canvas>
+
+      <FrameUpdate />
 
       <ambientLight intensity={0.3} />
       <directionalLight position={[1, 2, 3]} intensity={1} />
 
+      <OrbitControls enableZoom={false} />
+
+
+      <ScrollControls damping={0.25} pages={9}>
+
       <Office />
 
-      <FrameUpdate />
-
-      <OrbitControls enableZoom={false} enableRotate={false} enablePan={false} />
-
-      <group position={[0, 0, 0]}>
-        <group ref={trailRef}>
-          <PerspectiveCamera ref={cameraRef} makeDefault position={[2.3, 1.5, 2.3]}/>
-          <OrbitControls camera={cameraRef.current} enableZoom={false} />
+        <group ref={trailRef} position={[1, 1, 1]}>
+            <PerspectiveCamera ref={cameraRef} makeDefault position={[2.3, 1.5, 2.3]}/>
+            <OrbitControls camera={cameraRef.current} enableZoom={false} />
         </group>
-      </group>
+
+      </ScrollControls>
 
       </Canvas>
-
-
   )
 }
 
