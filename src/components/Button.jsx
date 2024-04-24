da602bf487fa6a20e601df9c46746d5697a1883f
Kimdekker <kim.dekker@q42.nl>
2024-04-24 16:25:28 +0200

    button on click to timeline

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 const Button = ({ text }) => {
   return (
-    <button className="button" type="button">{text}</button>
+    <button>{text}</button>
   )
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,34 @@
 import Space from '../components/Space'
 import Button from '../components/Button'
 import { Canvas } from '@react-three/fiber'
+import { useState } from 'react'
 
 
 const HomePage = () => {
 
+  const [view, setView] = useState('office');
+
+  const viewToTime = {
+    'office': 0,
+    'library': 3,
+    'attic': 5,
+  }
+
+  const handleButtonClick = (newView) => {
+    const time = viewToTime[newView];
+    setView(time);
+  }
+
   return (
       <>
       <Canvas>
-        <Space />
+        <Space view={view} />
       </Canvas>
 
       <div className='btns'>
-      <Button text={'Explore'}/>
-      <Button text={'About'}/>
+        <Button onClick={() => handleButtonClick('office')} href='#office' text={'The Office'} />
+        <Button onClick={() => handleButtonClick('library')} href='#library' text={'The Library'} />
+        <Button onClick={() => handleButtonClick('attic')} href='#attic' text={'The Attic'} />
       </div>
 
       </>
